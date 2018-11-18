17a4692d4a98eac0342f660db2c2e78700520d9f
Phan Dung Tri <phandungtri4work@gmail.com>
2018-11-18 21:16:35 +0700

    add Product class

---
+++ Printable.java
@@ -0,0 +1,4 @@
+public interface IPrintable
+{
+    public String toString();
+}
---
+++ Product.java
@@ -0,0 +1,67 @@
+public abstract class Product implements IPrintable
+{
+    /*Constructor*/
+    public Product(String id, Product.Type type) { 
+        this.id = new String("");
+        name = new String("");
+        code = new String("");
+        setID(id);
+        setType(type);
+        createProductInfo(); 
+    }
+
+    /*Inner objects*/
+    public enum Type
+    {
+        Card,
+        Game,
+    };
+
+    /*Members*/
+    String  id;
+    String  name;
+    String  code;
+    Type    type;
+    int     price;
+
+    /*Get methods*/
+    public String   getID() { return id; }
+    public String   getName() { return name; }
+    public String   getCode() { return code; }
+    public Type     getType() { return type; }
+    public int      getPrice() { return price; }
+
+    /*Set methods*/
+    public void     setID(String id) { this.id = id; }
+    public void     setName(String name) { this.name = name; }
+    public void     setCode(String code) { this.code = code; }
+    public void     setType(Type type) { this.type = type; }
+    public void     setPrice(int price) { this.price = price; }
+
+    /*Other methods*/
+    public abstract void createProductDetails();
+    
+    public void createProductInfo() {
+        System.out.print("Input product name: ");
+        setName(Shop.scanner.nextLine());
+
+        System.out.print("Input activation code: ");
+        setCode(Shop.scanner.nextLine());
+
+        System.out.print("Input product price: ");
+        setPrice(Shop.scanner.nextInt());
+        Shop.scanner.nextLine();
+
+        createProductDetails();
+    }
+
+    public String toString()
+    {
+        String str = new String("");
+        str += "ID: " + getID() + "\n" +
+               "Name: " + getName() + "\n" + 
+               "Type: " + getType().toString() + "\n" +
+               "Price: " + getPrice() + "\n";
+        return str;
+    }
+}
