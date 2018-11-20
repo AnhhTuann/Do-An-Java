11c5467a52b671b4aa79581f73ac7d9f8fe328de
Phan Dung Tri <phandungtri4work@gmail.com>
2018-11-20 22:51:53 +0700

    Implement Publisher class

--- Publisher.java
+++ Publisher.java
@@ -1,11 +1,16 @@
 import java.util.LinkedList;
 
-public class Publisher 
+public class Publisher implements IPrintable
 {
     /*Constructor*/
+    public Publisher(String name) {
+        this.name = name;
+        titleList = new LinkedList<String>();
+    }
+
     /*Members*/
-    String name;
-    LinkedList<String> productList;
+    private String name;
+    private LinkedList<String> titleList;
 
     /*Get methods*/
     public String getName() {
@@ -13,7 +18,11 @@ public class Publisher
     }
 
     public int getQuantity() {
-        return productList.size();
+        return titleList.size();
+    }
+
+    public int getTitleList() {
+        return titleList;
     }
 
     /*Set methods*/
@@ -22,9 +31,50 @@ public class Publisher
     }
 
     /*Other methods*/
-    public void addProduct(String name) {
-        productList.add(name);
+    public void addTitle(String name) {
+        if (findTitle(name) != -1)
+        {
+            System.out.println("Title is already exist!");
+            return;
+        }
+
+        titleList.add(name);
     }
 
+    public void removeTitle(String name) {
+        int index = findTitle(name);
+        if (index == -1)
+        {
+            System.out.println("Cannot find title!");
+            return;
+        }
 
+        titleList.remove(index);
+    }
+
+    public int findTitle(String name) {
+        for (String str : titleList)
+        {
+            if (str.equals(name))
+                return titleList.indexOf(str);
+        }
+
+        return -1;
+    }
+
+    public String toString() {
+        String str = new String("");
+        str += "\n----------\n" + 
+               "Name: " + getName() + "\n" +
+               "Title: ";
+        
+        for (String title : titleList)
+        {
+            str += title + "\t";
+        }
+
+        str += "\n----------\n";
+
+        return str;
+    }
 }
