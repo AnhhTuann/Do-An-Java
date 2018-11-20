589a374b70b20d8bf0bfda66312fe5efb373fd94
Phan Dung Tri <phandungtri4work@gmail.com>
2018-11-20 22:52:06 +0700

    Add Publisher List class

---
+++ PublisherList.java
@@ -0,0 +1,62 @@
+import java.util.LinkedList;
+
+public class PublisherList
+{
+    /*Constructor*/
+
+    /*Members*/
+    LinkedList<Publisher> list;
+
+    /*Get methods*/
+    public Publisher getPublisher(String name) {
+        int index = findPublisher(name);
+        if (index == -1)
+        {
+            System.out.println("Cannot find publisher!");
+            return null;
+        }
+
+        return list.get(index);
+    }
+
+    public int getQuantity() {
+        return list.size();
+    }
+
+    public int getQuantity(String name) {
+        return getPublisher(name).getQuantity();
+    }
+
+    /*Set methods*/
+    /*Other methods*/
+    public void addPublisher(String name) {
+        int index = findPublisher(name);
+        if (index != -1)
+        {
+            System.out.println("Publisher is already exist!");
+            return;
+        }
+        
+        Publisher publisher = new Publisher(name);
+        list.add(publisher);
+    }
+
+    public void removePublisher(String name) {
+        int index = findPublisher(name);
+        if (index != -1)
+        {
+            System.out.println("Publisher is already exist!");
+            return;
+        }
+
+        list.remove(index);
+    }
+
+    public int findPublisher(String name) { 
+        for (Publisher publisher : list) 
+            if (publisher.getName().equalsIgnoreCase(name))
+                return list.indexOf(publisher);
+        
+        return -1;
+    }
+}
