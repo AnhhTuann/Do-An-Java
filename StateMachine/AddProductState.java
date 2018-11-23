f23675460d5f758b46489d6efdb01a39c3d2d482
Phan Dung Tri <phandungtri4work@gmail.com>
2018-11-23 21:51:37 +0700

    Edit code

--- StateMachine/AddProductState.java
+++ StateMachine/AddProductState.java
@@ -16,37 +16,39 @@ public class AddProductState implements State<Shop>
     }
 
     /*Other methods*/
+    @Override
     public void enter(Shop owner) {
         System.out.print("\n--Which type of product do you want to add?\n1. Game\n2. Card\n3. Cancel\nInput: ");
     }
 
+    @Override
     public void execute(Shop owner) {
         switch (Shop.scanner.nextInt())
         {
             case 1:
-                {
-                    Shop.scanner.nextLine();
-                    System.out.print("\nGame chosen! Input ID: ");
-                    String id = Shop.scanner.nextLine();
-                    System.out.print("How many products do you want to add? ");
-                    int quantity = Shop.scanner.nextInt();
-                    Shop.scanner.nextLine();
-                    owner.getGameList().addProduct(id, quantity);
-                    owner.getStateMachine().changeState(this);
-                    break;
-                }
+            {
+                Shop.scanner.nextLine();
+                System.out.print("\nGame chosen! Input ID: ");
+                String id = Shop.scanner.nextLine();
+                System.out.print("How many products do you want to add? ");
+                int quantity = Shop.scanner.nextInt();
+                Shop.scanner.nextLine();
+                owner.getGameList().addProduct(id, quantity);
+                owner.getStateMachine().changeState(this);
+                break;
+            }
             case 2:
-                {
-                    Shop.scanner.nextLine();
-                    System.out.print("\nCard chosen! Input ID: ");
-                    String id = Shop.scanner.nextLine();
-                    System.out.print("How many products do you want to add? ");
-                    int quantity = Shop.scanner.nextInt();
-                    Shop.scanner.nextLine();
-                    owner.getCardList().addProduct(id, quantity);
-                    owner.getStateMachine().changeState(this);
-                    break;
-                }
+            {
+                Shop.scanner.nextLine();
+                System.out.print("\nCard chosen! Input ID: ");
+                String id = Shop.scanner.nextLine();
+                System.out.print("How many products do you want to add? ");
+                int quantity = Shop.scanner.nextInt();
+                Shop.scanner.nextLine();
+                owner.getCardList().addProduct(id, quantity);
+                owner.getStateMachine().changeState(this);
+                break;
+            }
             case 3:
                 owner.getStateMachine().changeState(StaffMenuState.getInstance());;
                 break;
@@ -56,5 +58,6 @@ public class AddProductState implements State<Shop>
         }
     }
 
+    @Override
     public void exit(Shop owner) {}
 }
