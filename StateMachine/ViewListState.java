feec91cee36c67392587686c74ee5f013565f1f7
Phan Dung Tri <phandungtri4work@gmail.com>
2018-11-23 22:04:09 +0700

    Fix code

--- StateMachine/ViewListState.java
+++ StateMachine/ViewListState.java
@@ -19,7 +19,7 @@ public class ViewListState implements State<Shop>
     /*Other methods*/
     @Override
     public void enter(Shop owner) {
-        System.out.print("\n--Which type of list do you want to add?\n1. Game List\n2. Card List\n3. Cancel\nInput: ");
+        System.out.print("\n--Which type of list do you want to view?\n1. Game List\n2. Card List\n3. Cancel\nInput: ");
     }
 
     @Override
